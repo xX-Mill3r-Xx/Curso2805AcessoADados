9f69b0eb1b8f8c0346855d302d591c296eeba261
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-17 20:55:31 -0300

    Project Create

---
+++ Aula001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Connection string*/
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+
+            /*Este é um exemplo de uma string de conexão para sqlServer 
+               Server= 'Nome do servidor,porta padrao';Database= 'Nome do banco'; User ID= 'Nome de usuario'; Password= 'Senha'*/
+        }
+    }
+}
---
+++ Curso2805Balta_IO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curso2805Balta_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Intro*/
+        }
+    }
+}
